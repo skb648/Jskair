1fac710493cf4424cadee553d3e3ebfd4720b2eb
skb648 <bairwask44@gmail.com>
2026-09-03 19:47:06 +0530

    fix(build): remove legacy Kotlin Android plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
 // Top-level build file.
-// Fix #84: remove hardcoded Hilt/KSP versions from buildscript classpath so
-// version bumps only need to happen in the version catalog (libs.versions.toml).
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.lint) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.hilt) apply false
