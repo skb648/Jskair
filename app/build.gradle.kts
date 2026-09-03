9a89ff7983c9bbed62591b54c11548ad56f30cc6
skb648 <bairwask44@gmail.com>
2026-09-03 19:46:58 +0530

    fix(build): migrate app to AGP 9 built-in Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
@@ -72,11 +71,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = false
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-        allWarningsAsErrors = false
     }
 
     buildFeatures {
